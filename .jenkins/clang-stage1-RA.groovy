d795b0dab4fc7fb5fcc9eedb9ab1b31c1c6f94f0
justiceadams <jadams23@apple.com>
2026-01-06 09:59:03 -0800

    [Green Dragon] Add clang-stage1-RA job

---
+++ .jenkins/clang-stage1-RA.groovy
@@ -0,0 +1,36 @@
+//ToDo: This will go away with the global shared library
+branchName = 'jadams/unified-templated-test'
+
+library identifier: "zorg-shared-lib@${branchName}",
+        retriever: modernSCM([
+            $class: 'GitSCMSource',
+            remote: "https://github.com/justice-adams-apple/llvm-zorg.git",
+            credentialsId: scm.userRemoteConfigs[0].credentialsId
+        ])
+
+clangPipeline(
+    jobName: env.JOB_NAME,
+    buildConfig: [
+        stage: 1,
+        build_type: 'cmake',
+        cmake_type: 'RelWithDebInfo',
+        assertions: true,
+        projects: 'clang;clang-tools-extra;compiler-rt',
+        timeout: 120,
+        incremental: false
+    ],
+
+    testConfig: [
+        test_type: 'testlong',
+        timeout: 120,
+        junit_patterns: [
+            "clang-build/**/testresults.xunit.xml"
+        ]
+    ]
+//     triggeredJobs: [
+//         'clang-stage2-cmake-RgSan_relay',
+//         'clang-stage2-Rthinlto_relay',
+//         'relay-lnt-ctmark',
+//         'relay-test-suite-verify-machineinstrs'
+//     ]
+)
