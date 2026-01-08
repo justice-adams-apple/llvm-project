fed48cf12ca6587d46bd94a5cc5445c573a81615
justiceadams <jadams23@apple.com>
2026-01-08 08:49:54 -0800

    [Green Dragon] Add clang-stage1-RA-cmake-incremental job

---
+++ .jenkins/clang-stage1-RA-cmake-incremental.groovy
@@ -0,0 +1,31 @@
+//ToDo: This will go away with the global shared library
+branchName = 'jadams/unified-templated-test'
+
+library identifier: "zorg-shared-lib@${branchName}",
+        retriever: modernSCM([
+            $class: 'GitSCMSource',
+            remote: "https://github.com/justice-adams-apple/llvm-zorg.git",
+            credentialsId: scm.userRemoteConfigs[0].credentialsId
+        ])
+
+clangPipeline(
+    jobName: env.JOB_NAME,
+    buildConfig: [
+        stage: 1,
+        build_type: 'cmake',
+        cmake_type: 'default',
+        assertions: true,
+        projects: 'clang',
+        timeout: 150,
+        incremental: true
+    ],
+
+    testConfig: [
+        test_type: 'testlong',
+        timeout: 150,
+        junit_patterns: [
+            "clang-build/**/testresults.xunit.xml"
+        ]
+    ],
+)
+
