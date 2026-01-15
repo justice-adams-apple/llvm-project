b441ff99200d3692fb5eeed970fd8211ac415a09
justiceadams <jadams23@apple.com>
2026-01-15 12:01:25 -0800

    add lnt-ctmark-aarch64-O0-g job

---
+++ .jenkins/lnt-ctmark-aarch64-O0-g.groovy
@@ -0,0 +1,19 @@
+//ToDo: use main branch
+branchName = 'jadams/unified-templated-test'
+
+library identifier: "zorg-shared-lib@${branchName}",
+        retriever: modernSCM([
+            $class: 'GitSCMSource',
+            remote: "https://github.com/justice-adams-apple/llvm-zorg.git",
+            credentialsId: scm.userRemoteConfigs[0].credentialsId
+        ])
+
+common.testsuite_pipeline(label: 'macos-x86_64') {
+    timeout(30) {
+        sh """
+LNT_FLAGS+=" -C config/tasks/cmake/caches/target-arm64-iphoneos.cmake"
+LNT_FLAGS+=" -C config/tasks/cmake/caches/opt-O0-g.cmake"
+config/tasks/task jenkinsrun config/tasks/lnt-ctmark.sh -a compiler="${params.ARTIFACT}" -D LNT_FLAGS="\${LNT_FLAGS}"
+        """
+    }
+}
