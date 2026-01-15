b3983d3f0dc3451d3108a9589d1fe32dab3b820c
justiceadams <jadams23@apple.com>
2026-01-15 08:40:11 -0800

    add clang-stage2-Rthinlto job

---
+++ .jenkins/clang-stage2-Rthinlto.groovy
@@ -0,0 +1,36 @@
+//ToDo: use main branch
+branchName = 'jadams/unified-templated-test'
+
+library identifier: "zorg-shared-lib@${branchName}",
+        retriever: modernSCM([
+            $class: 'GitSCMSource',
+            remote: "https://github.com/justice-adams-apple/llvm-zorg.git",
+            credentialsId: scm.userRemoteConfigs[0].credentialsId
+        ])
+
+clangPipeline([
+    jobName: env.JOB_NAME,
+    zorgBranch: branchName,
+    buildConfig: [
+        build_type: "clang",
+        cmake_type: "RelWithDebInfo",
+        thinlto: true,
+        projects: "clang",
+        runtimes: "libunwind;compiler-rt",
+        stage: 2,
+        timeout: 1200,
+        incremental: false,
+        stage1Job: 'clang-stage1-RA',
+        cmake_flags: [
+            "-DCMAKE_DSYMUTIL=\${WORKSPACE}/host-compiler/bin/dsymutil"
+        ]
+    ],
+    testConfig: [
+        test_command: "clang",
+        test_type: "test",
+        timeout: 420,
+        junit_patterns: [
+            "clang-build/**/testresults.xunit.xml"
+        ]
+    ]
+])
